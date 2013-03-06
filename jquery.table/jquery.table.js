17252394acdbbff0aa40d802ec39cab002f3bb8e
chromero <christophe.romero@gmail.com>
2013-03-06 23:01:53 +0100

    ajout des actions + changement de certains click() par on ('click',...

--- jquery.table/jquery.table.js
+++ jquery.table/jquery.table.js
@@ -26,12 +26,12 @@
     $.widget("ui.table", {
         options: {
             name: "table",
-            template: {
-                navbar_pre: '<div id="$id_controlBar"><ul id="$id_navBar" style="list-style:none;margin: 0;">',
-                navbar_post: '</ul></div><div style="clear:both;"></div>',
-                navbaritem: '<li class="table_controls ui-state-default ui-corner-all" id="$id_$nom" style="float:left;margin-right:2px;text-decoration: none;"><span class="ui-widget ui-corner-all ui-icon ui-button $class" title="$nom"></span></li>',
-                column_header: '<th field="$field"><div class="table_header_div">$field_name</div><div class="table_filter ui-icon ui-icon-search"></div><div class="table_sort ui-icon ui-icon-triangle-2-n-s"></div></th>'
-            },
+            navbar_pre: '<div id="$id_controlBar"><ul id="$id_navBar" style="list-style:none;margin: 0;">',
+            navbar_post: '</ul></div><div style="clear:both;"></div>',
+            navbaritem: '<li class="table_controls ui-state-default ui-corner-all" id="$id_$nom" style="float:left;margin-right:2px;text-decoration: none;"><span class="ui-widget ui-corner-all ui-icon ui-button $class" title="$nom"></span></li>',
+            column_header: '<th field="$field"><div class="table_header_div">$field_name</div><div class="table_filter ui-icon ui-icon-search"></div><div class="table_sort ui-icon ui-icon-triangle-2-n-s"></div></th>',
+            action_pre: '<td><span class="table_action ui-icon ui-widget ui-button $class" action="$action">',
+            action_post: '</span></td>',
             default_controls: [{
                     id: 'first',
                     icon: 'ui-icon-arrowthickstop-1-w'
@@ -60,7 +60,8 @@
             evenclass: 'table_even',
             sort: null, // tri
             columns: null, // liste des colonnes
-            ajaxLoading: 0          // pour l'icone
+            ajaxLoading: 0, // pour l'icone
+            actions: []      // liste des actions ex : [{action:'test',class:'ui-icon-close'}]
         },
         _init: function() {
         },
@@ -76,8 +77,8 @@
             this.options.controls = this.options.default_controls.concat(this.options.controls);
             // creation de la barre de navigation
             // ajout du spinner, puis des controles
-             var chaine = this._template("navbar_pre");
-            chaine += this._getHtml("li", "spinner"," class='table_spinner'");
+            var chaine = this._template("navbar_pre");
+            chaine += this._getHtml("li", "spinner", " class='table_spinner'");
             chaine += "<span class='ui-widget ui-corner-all ui-icon ui-button' ></span></li>";
 
             var ctrls = this.options.controls;
@@ -98,13 +99,19 @@
             // récupérer les données
             this.getValues();
             var self = this;
-            // tous les controles ont la classe <nom>_controls
+            // tous les controles ont la classe table_controls
             // on leur rajoute l'appel de la methode correspondante
-            $("#"+this._getId('navBar')+" .table_controls").click(
+            $("#" + this._getId('navBar')).on('click', ".table_controls",
                     function() {
                         var methode = $(this).attr('id').replace(new RegExp('^' + self.options.name + '_'), '');
                         self[methode]();
                     });
+            $("#" + this._getId('table')).on('click', '.table_action', function(event) {
+                var action = $(this).attr('action');
+                self.call(event.target ,action);
+                //$(this._getId('table'))[action]();
+            });
+
         },
         destroy: function() {
             // on restaure le contenu original
@@ -164,9 +171,9 @@
         },
         _createHeader: function() {
             // on supprime l'ancien si besoin
-            var ctrl_id = '#' + this._getId('header');
+            var $ctrl_id = $('#' + this._getId('header'));
 
-            $(ctrl_id).empty();
+            $ctrl_id.empty();
             var chaine = "";
             // [{id:xxx,label:xxx,type:xxx},{....}]
             // ou
@@ -186,7 +193,7 @@
                 }
                 cols = cols[i];
             }
-            $(chaine).insertBefore($(ctrl_id));
+            $(chaine).insertBefore($ctrl_id);
             var self = this;
             var sort;
             var filter;
@@ -197,10 +204,13 @@
             for (var i = 0; i < cols.length; i++) {
                 chaine += "<th field='" + cols[i]['id'] + "'><div class='title_div'>" + cols[i]['label'] + "</div>" + filter + sort + '</th>';
             }
-            $('#' + this._getId('controlBar') + ' td').attr('colspan', cols.length);
-            $(ctrl_id).append(chaine);
+            var nbActions = this.options.actions.length;
+            if (nbActions > 0) {
+                chaine += '<th colspan="' + nbActions + '">actions</th>';
+            }
+            $ctrl_id.append(chaine);
             // click sur le titre pour tri
-            $(ctrl_id + ' .title_div').click(function() {
+            $ctrl_id.on('click', '.title_div', function() {
                 var current_sort = '';
                 if (self.options.sort !== null) {
                     current_sort = self.options.sort.split(' ');
@@ -223,9 +233,10 @@
                 self._setOption('sort', (champ + ' ' + sens));
             });
             // click sur filtrer = appel de showFilter
-            $(ctrl_id+' .filter').click(function() {
+            $ctrl_id.on('click', '.filter', function() {
                 self.showFilter($(this));
             });
+
         },
         // affiche un input sur le header correspondant
         showFilter: function(element) {
@@ -233,7 +244,7 @@
             var field = element.parent().attr('field');
             var html = '<INPUT id="' + this._getId('input') + '" class="filter_input" type="text" />';
             $('#' + this._getId('header')).append(html);
-            var $input=$('#' + this._getId('input'));
+            var $input = $('#' + this._getId('input'));
             $input.width(element.parent().width() - 4).height(element.parent().children(':first').height() - 4).position({
                 my: 'center',
                 at: 'center',
@@ -276,21 +287,37 @@
                 $('#' + this._getId('spinner')).css('visibility', 'hidden');
             }
         },
+        call: function(element, method) {
+            var tr = $(element).closest('tr');
+            var data = tr.data('table_data');
+            this.options[method](data, tr);
+        },
         // on a récupéré les valeurs (donnée en ajax)
         // on met à jour l'écran
         _setValues: function(values) {
             var chaine = '';
-            $('.' + this._getId("values")).remove();
+            // TODO ajouter les actions
+            var chaineAction = "";
+            if (this.options.actions.length > 0) {
+                for (var k in this.options.actions) {
+                    chaineAction += this._template('action_pre', {class: this.options.actions[k].icon, action: this.options.actions[k].name});
+                }
+            }
+            //$('.' + this._getId("values")).remove();
+            var $table = $('#' + this._getId('table'));
+            $table.find('.table_values').remove();
             for (var i = 0; i < values.length; i++) {
-                chaine += "<tr class='" + this._getId("values") + "'>";
+                chaine = "<tr id='lg_"+i+"' class='table_values'>";
                 for (var j = 0; j < values[i].length; j++) {
                     chaine += '<td>' + values[i][j] + '</td>';
                 }
-                chaine += '</tr>';
+                chaine += chaineAction += '</tr>';
+                $table.append(chaine);
+                $('#lg_'+i).data('table_data',values[i]);
             }
-            $('#' + this._getId("table")).append(chaine);
-            $('.' + this._getId("values") + ':odd').addClass(this.options.oddclass);
-            $('.' + this._getId("values") + ':even').addClass(this.options.evenclass);
+            $table.find('.table_values:odd').addClass(this.options.oddclass);
+            $table.find('.table_values:even').addClass(this.options.evenclass);
+
         },
         _setSort: function(value) {
             // on a mis à jour le tri, on récupère les données
@@ -328,10 +355,10 @@
             if (typeof(valeurs) === 'undefined') {
                 valeurs = {};
             }
-            var chaine = this.options.template[nom];
+            var chaine = this.options[nom];
             var values = $.extend(valeurs, {id: this.options.name});
             for (var keys in values) {
-                var regexp = new RegExp('\\$' + keys,'g');
+                var regexp = new RegExp('\\$' + keys, 'g');
                 chaine = chaine.replace(regexp, values[keys]);
             }
             return chaine;
